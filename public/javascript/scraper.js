0a6c0187663779a1725e12777edb66a6a6d9768b
David <davwil38@gmail.com>
2019-09-07 13:41:35 -0400

    cleaned up code

--- public/javascript/scraper.js
+++ public/javascript/scraper.js
@@ -1,10 +1,8 @@
-//On page "scraper", activates when user clicks button to get more articles
 $("#web-scraper").on("click", function () {
     $("#scraper-status").empty();
     $("#scraper-redirect").empty();
     $("#article-link").empty();
     $.post("/scraper")
-        //After the post is made, a visual display is made to show when data is retrived
         .then(function (data) {
             $("#scraper-status").empty();
             $("#scraper-status").html("Looking for new articles...")
@@ -19,13 +17,11 @@ $("#web-scraper").on("click", function () {
             }, 7000);
         });
 });
-// Displays retrieved articles on webpage
 $.getJSON("/api/articles", function (data) {
     for (var x = 0; x < data.length; x++) {
         $("#article-list").append("<p data-id='" + data[x]._id + "' class='article-title'>" + data[x].title + "<br />" + data[x].link + "</p>")
     };
 });
-// Note display on article list click
 $(document).on("click", ".article-title", function () {
     $("#note-div").empty();
     var thisId = $(this).attr("data-id");
@@ -45,15 +41,12 @@ $(document).on("click", ".article-title", function () {
             $("#body-text").append("<textarea class='form-control' rows='5' id='body-input' name='body'></textarea>");
             $("#note-content").append("<div class='col-md-12' id='note-save'><div>");
             $("#note-save").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-            
-
             if (data.note) {
                 $("#title-input").val(data.note.title);
                 $("#body-input").val(data.note.body);
             };
         });
 });
-// Adds created note into the mongo db
 $(document).on("click", "#savenote", function () {
     var thisId = $(this).attr("data-id");
     $.ajax({
